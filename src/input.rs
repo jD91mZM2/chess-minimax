781afbe262225e5994934be5e01b950066ebc103
jD91mZM2 <me@krake.one>
2018-04-12 06:51:43 +0200

    Separate input handling functions

--- src/input.rs
+++ src/input.rs
@@ -18,28 +18,188 @@ fn positions_join<'a, I: Iterator<Item = &'a Pos>>(input: I) -> String {
 	output
 }
 
+pub fn print_prompt(board: &mut Board) {
+	println!();
+	#[cfg(not(feature = "white"))]
+	match check_status(board) {
+		CheckStatus::None => {},
+		CheckStatus::CheckMine(mate) => println!("BLACK CHECK{}\n", if mate { "MATE" } else { "ED" }),
+		CheckStatus::CheckYour(mate) => println!("WHITE CHECK{}\n", if mate { "MATE" } else { "ED" }),
+	}
+	#[cfg(feature = "white")]
+	match check_status(board) {
+		CheckStatus::None => {},
+		CheckStatus::CheckMine(mate) => println!("WHITE CHECK{}\n", if mate { "MATE" } else { "ED" }),
+		CheckStatus::CheckYour(mate) => println!("BLACK CHECK{}\n", if mate { "MATE" } else { "ED" }),
+	}
+	println!("{}", board::board_string(board));
+
+	println!("Commands: move(f), spawn, clear, reset, score, possible, all, best");
+	print!("> ");
+	io::stdout().flush().unwrap();
+}
+pub fn apply_input(board: &mut Board, cmd: &str) {
+	let mut args = cmd.split_whitespace();
+	let cmd = match args.next() {
+		Some(cmd) => cmd,
+		None => return,
+	};
+	let args: Vec<_> = args.collect();
+
+	macro_rules! usage {
+		($n:expr, $usage:expr) => {
+			if args.len() != $n {
+				eprintln!($usage);
+				eprintln!("Incorrect arguments");
+				return;
+			}
+		}
+	}
+	macro_rules! parse_pos {
+		($input:expr) => {
+			match parse_position($input) {
+				Some(pos) => pos,
+				None => {
+					eprintln!("Invalid position");
+					return;
+				}
+			}
+		}
+	}
+	match cmd {
+		"move" | "movef" => {
+			usage!(2, "move(f) <from> <to>");
+
+			let force = cmd == "movef";
+
+			let from = parse_pos!(args[0]);
+			let to = parse_pos!(args[1]);
+
+			if !force {
+				let piece = board_get(board, from);
+
+				if piece.is_mine() {
+					eprintln!("Can't move with that piece! It's mine!");
+					eprintln!("TIP: movef moves without checking first.");
+					return;
+				}
+
+				let mut found = false;
+				for m in &piece.possible_moves(board, from) {
+					if *m == to {
+						found = true;
+					}
+				}
+
+				if !found {
+					eprintln!("Can't move there!");
+					eprintln!("TIP: movef moves without checking first.");
+					return;
+				}
+			}
+
+			let (diff, _) = board_move(board, from, to);
+
+			if !force {
+				let possible = possible_moves(board, true);
+				if get_check(board, false, &possible).is_some() {
+					eprintln!("Can't move there! You'd place yourself in check!");
+					eprintln!("TIP: movef moves without checking first.");
+
+					board_apply(board, diff);
+					return;
+				}
+			}
+		},
+		"spawn" => {
+			usage!(2, "spawn <position> <piece>");
+
+			let pos = parse_pos!(args[0]);
+			let piece = match args[1].parse() {
+				Ok(piece) => piece,
+				Err(_) => {
+					eprintln!("No such piece");
+					return;
+				}
+			};
+
+			board_set(board, pos, piece);
+		},
+		"clear" => {
+			usage!(0, "clear");
+
+			for line in board {
+				for piece in line.iter_mut() {
+					*piece = Piece::Empty;
+				}
+			}
+		},
+		"reset" => {
+			usage!(0, "reset");
+
+			*board = board::make_board();
+		},
+		"score" => {
+			usage!(0, "score");
+
+			println!("{}", score(board));
+		}
+		"possible" => {
+			usage!(1, "possible <pos>");
+
+			let pos = parse_pos!(args[0]);
+
+			let possible = board_get(board, pos).possible_moves(board, pos);
+			if possible.is_empty() {
+				println!("No possible moves");
+				return;
+			}
+
+			println!("{}", positions_join(possible.iter()));
+		},
+		"all" => {
+			usage!(0, "all");
+
+			let possible = possible_moves(board, true);
+			if possible.is_empty() {
+				println!("No possible moves");
+				return;
+			}
+
+			for ((x, y), moves) in possible {
+				if moves.is_empty() {
+					return;
+				}
+
+				let pos = position_string((x, y));
+				println!("{}: {}", pos, positions_join(moves.iter()));
+			}
+		},
+		"best" => {
+			usage!(0, "best");
+
+			#[cfg(feature = "cpuprofiler")]
+			PROFILER.lock().unwrap().start("crappy-chess-minimax.profile").unwrap();
+
+			let (score, from, to) = start_search(board);
+
+			#[cfg(feature = "cpuprofiler")]
+			PROFILER.lock().unwrap().stop().unwrap();
+
+			board_move(board, from, to);
+
+			println!("Final Score: {}", score);
+			println!("Move {} to {}", position_string(from), position_string(to));
+		},
+		_ => eprintln!("Unknown command"),
+	}
+}
+
 pub fn main() {
 	let mut board = make_board();
 
 	loop {
-		println!();
-		#[cfg(not(feature = "white"))]
-		match check_status(&mut board) {
-			CheckStatus::None => {},
-			CheckStatus::CheckMine(mate) => println!("BLACK CHECK{}\n", if mate { "MATE" } else { "ED" }),
-			CheckStatus::CheckYour(mate) => println!("WHITE CHECK{}\n", if mate { "MATE" } else { "ED" }),
-		}
-		#[cfg(feature = "white")]
-		match check_status(&mut board) {
-			CheckStatus::None => {},
-			CheckStatus::CheckMine(mate) => println!("WHITE CHECK{}\n", if mate { "MATE" } else { "ED" }),
-			CheckStatus::CheckYour(mate) => println!("BLACK CHECK{}\n", if mate { "MATE" } else { "ED" }),
-		}
-		println!("{}", board::board_string(&board));
-
-		println!("Commands: move(f), spawn, clear, reset, score, possible, all, best");
-		print!("> ");
-		io::stdout().flush().unwrap();
+		print_prompt(&mut board);
 
 		let mut cmd = String::new();
 		match io::stdin().read_line(&mut cmd) {
@@ -51,159 +211,6 @@ pub fn main() {
 			}
 		};
 
-		let mut args = cmd.split_whitespace();
-		let cmd = match args.next() {
-			Some(cmd) => cmd,
-			None => continue,
-		};
-		let args: Vec<_> = args.collect();
-
-		macro_rules! usage {
-			($n:expr, $usage:expr) => {
-				if args.len() != $n {
-					eprintln!($usage);
-					eprintln!("Incorrect arguments");
-					continue;
-				}
-			}
-		}
-		macro_rules! parse_pos {
-			($input:expr) => {
-				match parse_position($input) {
-					Some(pos) => pos,
-					None => {
-						eprintln!("Invalid position");
-						continue;
-					}
-				}
-			}
-		}
-		match cmd {
-			"move" | "movef" => {
-				usage!(2, "move(f) <from> <to>");
-
-				let force = cmd == "movef";
-
-				let from = parse_pos!(args[0]);
-				let to = parse_pos!(args[1]);
-
-				if !force {
-					let piece = board_get(&board, from);
-
-					if piece.is_mine() {
-						eprintln!("Can't move with that piece! It's mine!");
-						eprintln!("TIP: movef moves without checking first.");
-						continue;
-					}
-
-					let mut found = false;
-					for m in &piece.possible_moves(&board, from) {
-						if *m == to {
-							found = true;
-						}
-					}
-
-					if !found {
-						eprintln!("Can't move there!");
-						eprintln!("TIP: movef moves without checking first.");
-						continue;
-					}
-				}
-
-				let (diff, _) = board_move(&mut board, from, to);
-
-				if !force {
-					let possible = possible_moves(&board, true);
-					if get_check(&board, false, &possible).is_some() {
-						eprintln!("Can't move there! You'd place yourself in check!");
-						eprintln!("TIP: movef moves without checking first.");
-
-						board_apply(&mut board, diff);
-						continue;
-					}
-				}
-			},
-			"spawn" => {
-				usage!(2, "spawn <position> <piece>");
-
-				let pos = parse_pos!(args[0]);
-				let piece = match args[1].parse() {
-					Ok(piece) => piece,
-					Err(_) => {
-						eprintln!("No such piece");
-						continue;
-					}
-				};
-
-				board_set(&mut board, pos, piece);
-			},
-			"clear" => {
-				usage!(0, "clear");
-
-				for line in &mut board {
-					for piece in line.iter_mut() {
-						*piece = Piece::Empty;
-					}
-				}
-			},
-			"reset" => {
-				usage!(0, "reset");
-
-				board = board::make_board();
-			},
-			"score" => {
-				usage!(0, "score");
-
-				println!("{}", score(&board));
-			}
-			"possible" => {
-				usage!(1, "possible <pos>");
-
-				let pos = parse_pos!(args[0]);
-
-				let possible = board_get(&board, pos).possible_moves(&board, pos);
-				if possible.is_empty() {
-					println!("No possible moves");
-					continue;
-				}
-
-				println!("{}", positions_join(possible.iter()));
-			},
-			"all" => {
-				usage!(0, "all");
-
-				let possible = possible_moves(&board, true);
-				if possible.is_empty() {
-					println!("No possible moves");
-					continue;
-				}
-
-				for ((x, y), moves) in possible {
-					if moves.is_empty() {
-						continue;
-					}
-
-					let pos = position_string((x, y));
-					println!("{}: {}", pos, positions_join(moves.iter()));
-				}
-			},
-			"best" => {
-				usage!(0, "best");
-
-				#[cfg(feature = "cpuprofiler")]
-				PROFILER.lock().unwrap().start("crappy-chess-minimax.profile").unwrap();
-
-				let (score, from, to) = start_search(&mut board);
-
-				#[cfg(feature = "cpuprofiler")]
-				PROFILER.lock().unwrap().stop().unwrap();
-
-				board_move(&mut board, from, to);
-
-				println!("Final Score: {}", score);
-				println!("Move {} to {}", position_string(from), position_string(to));
-			},
-			_ => eprintln!("Unknown command"),
-		}
+		apply_input(&mut board, &cmd);
 	}
 }
